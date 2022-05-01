7f7186eb1ff32f51b64e49d5af76f059da9d80c4
Ech0B0lt <echoesandbolts@gmail.com>
2022-05-01 16:05:43 +0800

    updated db.py
    - added library imports
    - added MongoDB connections
    - added two functions (add_stock_data(), add_stock_info())

---
+++ db.py
@@ -0,0 +1,23 @@
+from datetime import *
+from bs4 import BeautifulSoup
+import pymongo
+import json
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import talib as ta
+import requests
+
+# MongoDB Connection
+client = pymongo.MongoClient("mongodb://localhost:27017")
+db = client["wallstreetbeggars"]
+col_stock_data = db["stock_data"]
+col_stock_info = db["stock_info"]
+
+
+def add_stock_data():
+    pass
+
+
+def add_stock_info():
+    pass
