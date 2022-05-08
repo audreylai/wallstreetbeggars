56b93dc3e808af5f672d598e280be23dae6185f6
Lucas Ma <lucasma09202@gmail.com>
2022-05-08 16:06:17 +0800

    added chart

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import datetime
 from db import *
 
 app = Flask(__name__)
@@ -6,11 +7,59 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("home.html")
+	data = process_cdl(get_stock_data('0005-HK', 60))
+	return render_template("home.html", data=data)
 
 @app.route("/", methods=["POST"])
 def get_data():
-  return render_template("home.html")
+	return render_template("home.html")
+
+def process_cdl(data):
+	out = {
+		'sma_10': [],
+		'sma_20': [],
+		'sma_50': [],
+		'cdl': [],
+		'vol': [],
+		'vol_color': []
+	}
+	max_vol = 0
+	
+	for i in data:
+		if i['Volume'] > max_vol:
+			max_vol = i['Volume']
+
+		out['cdl'].append({
+			'x': datetime.timestamp(i['Date']) * 1000,
+			'o': i['Open'],
+			'h': i['High'],
+			'l': i['Low'],
+			'c': i['Close']
+		})
+		out['sma_10'].append({
+			'x': datetime.timestamp(i['Date']) * 1000,
+			'y': i['SMA10']
+		})
+		out['sma_20'].append({
+			'x': datetime.timestamp(i['Date']) * 1000,
+			'y': i['SMA20']
+		})
+		out['sma_50'].append({
+			'x': datetime.timestamp(i['Date']) * 1000,
+			'y': i['SMA50']
+		})
+		out['vol'].append({
+			'x': datetime.timestamp(i['Date']) * 1000,
+			'y': i['Volume']
+		})
+
+		if i['Open'] > i['Close']:
+			out['vol_color'].append('rgba(215,85,65,0.4)')
+		else:
+			out['vol_color'].append('rgba(80,160,115,0.4)')
+
+	out['max_vol'] = max_vol
+	return out
 
 @app.route("/rules")
 def rules():
@@ -21,4 +70,4 @@ def stock_info():
   return render_template("stock-info.html")
 
 if __name__ == "__main__":
-  app.run(port="5000", debug=True)
+	app.run(port="5000", debug=True)
