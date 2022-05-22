f115dee81065e8b035b964c30177adc7766cf416
aetherxt <adatanium@gmail.com>
2022-05-22 15:06:32 +0800

    moved function into db,py

--- app.py
+++ app.py
@@ -14,43 +14,6 @@ def home():
 def get_data():
 	return render_template("home.html")
 
-def process_cdl(data):
-	out = {
-		'sma10': [],
-		'sma20': [],
-		'sma50': [],
-		'cdl': [],
-		'vol': [],
-		'vol_color': []
-	}
-	max_vol = 0
-	
-	for i in data:
-		if i['volume'] > max_vol:
-			max_vol = i['volume']
-
-		out['cdl'].append({
-			'x': datetime.timestamp(i['date']) * 1000,
-			'o': i['open'],
-			'h': i['high'],
-			'l': i['low'],
-			'c': i['close']
-		})
-
-		for sma in ['sma10', 'sma20', 'sma50']:
-			out[sma].append({
-				'x': datetime.timestamp(i['date']) * 1000,
-				'y': i[sma]
-			})
-
-		if i['open'] > i['close']:
-			out['vol_color'].append('rgba(215,85,65,0.4)')
-		else:
-			out['vol_color'].append('rgba(80,160,115,0.4)')
-
-	out['max_vol'] = max_vol
-	return out
-
 @app.route("/rules")
 def rules():
 	return render_template("rules.html")
