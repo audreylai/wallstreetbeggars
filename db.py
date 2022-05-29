46a0ab9357e23241de928a8c1fdd19e48fde27e1
Audrey Lai <68845532+audreylai@users.noreply.github.com>
2022-05-29 15:47:25 +0800

    stock-info layout

--- db.py
+++ db.py
@@ -200,7 +200,7 @@ def get_stock_info(ticker):
     if ticker == "all":
         res = {
             "table": col_stock_info.find({}, {"_id": 0}),
-            "last_update": col_stock_info.find_one({"lastupdated":{"$exists": True}})["lastupdated"],
+            "last_update": col_stock_info.find_one({"last_updated":{"$exists": True}})["last_updated"],
             "industries": col_stock_info.distinct("industry")
             }
     else:
