57bf9c97d4ec08bf612291a8025fa7cf9aad1643
aetherxt <adatanium@gmail.com>
2022-06-12 15:42:53 +0800

    db hang seng index

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
 from db import *
 from pprint import pprint
 
-# pprint(get_stock_data('0005-HK', 60))
-# pprint(process_industry_avg(get_industry_close_pct('Banks', 60)))
-# add_stock_data_batch()
-# print(yfinance_info([f'000{str(i)}-HK' for i in range(1,3)]))
-
-# pprint(get_stock_info('0005-HK'))
-# print(get_stock_info('ALL'))
+add_stock_data_batch()
+add_hsi_data()
