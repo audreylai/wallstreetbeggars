7655c78e448b27db481864a85368a118a499e50a
Audrey Lai <68845532+audreylai@users.noreply.github.com>
2022-06-17 12:57:41 +0800

    add api routes back into app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-import api
+# import api
 from db import *
 
 
@@ -126,6 +126,56 @@ def stock_analytics():
 
 	return render_template("stock-analytics.html", stock_data=stock_data, stock_info=stock_info, industries=get_all_industries(), indexes=get_all_tickers(ticker_type='index'))
 
+@app.route("/api/get_stock_data", methods=['GET'])
+def api_get_stock_data():
+	ticker = request.args.get('ticker', type=str)
+	period = request.args.get("period", type=int)
+	interval = request.args.get("interval", type=int)
+
+	if period is None or period <= 0: return {}, 400
+	if interval is None or interval <= 0: return {}, 400
+	if ticker is None: 
+		return {}, 400
+	else:
+		ticker = ticker.upper().replace(".", "-")
+
+	data = process_stock_data(get_stock_data(ticker, period), interval)
+	data['ticker'], data['period'], data['interval'] = ticker, period, interval
+	return json.dumps(data)
+
+
+@app.route("/api/get_stock_close_pct", methods=['GET'])
+def api_get_stock_close_pct():
+	ticker = request.args.get('ticker', type=str)
+	period = request.args.get("period", type=int)
+	interval = request.args.get("interval", type=int)
+
+	if period is None or period <= 0: return {}, 400
+	if interval is None or interval <= 0: return {}, 400
+	if ticker is None: 
+		return {}, 400
+	else:
+		ticker = ticker.upper().replace(".", "-")
+
+	data = process_stock_data(get_stock_data(
+		ticker, period), interval, include=['close_pct'])
+	data['ticker'], data['period'], data['interval'] = ticker, period, interval
+	return data
+
+
+@app.route("/api/get_industry_close_pct", methods=['GET'])
+def api_get_industry_close_pct():
+	industry = request.args.get('industry', type=str)
+	period = request.args.get("period", type=int)
+	interval = request.args.get("interval", type=int)
+
+	if period is None or period <= 0: return {}, 400
+	if interval is None or interval <= 0: return {}, 400
+	if industry is None: return {}, 400
+	
+	data = process_industry_avg(get_industry_close_pct(industry, period), interval)
+	data['industry'], data['period'], data['interval'] = industry, period, interval
+	return data
 
 if __name__ == "__main__":
 	app.run(port="5000", debug=True)
