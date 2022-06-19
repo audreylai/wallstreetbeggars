7770e6e007b12e930c35adf61fa3e5a2e66c9be5
Audrey Lai <68845532+audreylai@users.noreply.github.com>
2022-06-19 16:37:05 +0800

    strength index graph

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from db_utils import *
 from utils import *
 from pprint import pprint
 
-# add_stock_data_batch()
+add_stock_data_batch()
 # add_stock_info_batch()
 # pprint(get_stock_data('0005-HK', 180))
 # print(get_all_tickers(ticker_type='index'))
