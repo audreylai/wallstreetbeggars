cb9dcc3676725fbb271e1e25d4818b528b4bdc8c
Audrey Lai <68845532+audreylai@users.noreply.github.com>
2022-06-26 15:06:13 +0800

    market cap min filter

--- app.py
+++ app.py
@@ -51,6 +51,8 @@ def rules_edit():
 def stock_list_page():
 	page = request.values.get("page", type=int, default=1)
 	filter_industry = request.values.get("filter_industry", type=str, default='')
+	filter_mkt_cap = request.values.get("min_mkt_cap", type=int, default=9)
+	print(filter_mkt_cap)
 	sort_col = request.values.get("sort_col", type=str, default='ticker')
 	sort_dir_str = request.values.get("sort_dir", type=str, default='asc')
 	sort_dir = pymongo.DESCENDING if sort_dir_str == 'desc' else pymongo.ASCENDING
@@ -69,7 +71,7 @@ def stock_list_page():
 
 	active_tickers = get_active_tickers("test")['active']
 	last_updated = stock_table['last_updated'].strftime("%d/%m/%Y")
-	return render_template("stock-list.html", stock_table=stock_table['table'], last_updated=last_updated, industries=stock_table['industries'], active_tickers=active_tickers, num_of_pages=num_of_pages, page=page, filter_industry=filter_industry, sort_col=sort_col, sort_dir=sort_dir)
+	return render_template("stock-list.html", stock_table=stock_table['table'], last_updated=last_updated, industries=stock_table['industries'], active_tickers=active_tickers, num_of_pages=num_of_pages, page=page, filter_industry=filter_industry, sort_col=sort_col, sort_dir=sort_dir, filter_mkt_cap=filter_mkt_cap)
 
 
 # this should be an api
