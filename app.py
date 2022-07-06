0fc63aa5be0652a493bb5526c3dc73e1af766524
Audrey Lai <68845532+audreylai@users.noreply.github.com>
2022-07-06 18:55:26 +0800

    industries pages minor fixes

--- app.py
+++ app.py
@@ -128,10 +128,10 @@ def industries():
 	industries = get_all_industries()
 	print(industry_details.keys())
 
-	industry_detail = {}
-	if request.method == "POST":
+	industry_detail = {"Banks": industry_details["Banks"] }
+	if request.method == "POST" and request.values.get("industry_detail") in industry_details:
 		industry_detail = {request.values.get("industry_detail"): industry_details[request.values.get("industry_detail")]}
-		print(industry_detail)
+
 	return render_template("industries.html", dark_mode=dark_mode, table_data=table_data, industry_details=industry_details, industries=industries, industry_detail=industry_detail)
 
 # this should be an api
