4dffe8a52c8ae6b7f27c16c64b3ec1a484d24d84
Ech0B0lt <echoesandbolts@gmail.com>
2022-07-22 21:48:17 +0800

    added cdl pattern rule checking
    - save_rules_results() now checks for cdl pattern results
    - get_rules_results() has been modified such that u can now choose whether to get cdl pattern rules or tech ind rules

--- db_pkg/rules.py
+++ db_pkg/rules.py
@@ -8,7 +8,7 @@ col_rules_results = db["rules_results"]
 
 
 def get_rules(username):
-	return col_users.find_one({"username": username}, {"_id": 0, "buy": 1, "sell": 1})
+	return col_users.find_one({"username": username}, {"_id": 0, "buy": 1, "sell": 1, "cdl_buy": 1, "cdl_sell": 1})
 
 
 def update_rules(username, buy, sell, cdl):
@@ -17,8 +17,12 @@ def update_rules(username, buy, sell, cdl):
 	else:
 		col_users.update_one({"username": username}, {"$set": {"buy": buy, "sell": sell}})
 
-def get_rules_results(ticker):
-	cursor = col_rules_results.find({"ticker": ticker}, {"_id": 0, "hit_buy_rules": 1, "hit_sell_rules": 1, "miss_buy_rules": 1, "miss_sell_rules": 1})
+
+def get_rules_results(ticker, cdl):
+	if cdl:
+		cursor = col_rules_results.find({"ticker": ticker}, {"_id": 0, "hit_cdl_buy_rules": 1, "hit_cdl_sell_rules": 1, "miss_cdl_buy_rules": 1, "miss_cdl_sell_rules": 1})
+	else:
+		cursor = col_rules_results.find({"ticker": ticker}, {"_id": 0, "hit_buy_rules": 1, "hit_sell_rules": 1, "miss_buy_rules": 1, "miss_sell_rules": 1})
 	return cursor.next()
 
 
@@ -40,15 +44,36 @@ def save_rules_results(limit=100):
 	
 		last_stock_data = stock.get_last_stock_data(ticker)
 		user_rules = get_rules("test")
+		# print(user_rules)
+
+		# Parse regular tech indicator rules
 		parsed_buy_rules, parsed_sell_rules = utils.parse_rules(user_rules["buy"], user_rules["sell"])
 		hit_buy_rules, hit_sell_rules, miss_buy_rules, miss_sell_rules = utils.format_rules(*utils.get_hit_miss_rules(last_stock_data, parsed_buy_rules, parsed_sell_rules))
 
+		# Check CDL pattern rules
+		last_stock_cdl_pattern = stock.get_ticker_last_cdl_pattern(ticker)
+		hit_cdl_buy_rules, miss_cdl_buy_rules, hit_cdl_sell_rules, miss_cdl_sell_rules = ([] for i in range(4))
+		cdl_buy, cdl_sell = user_rules["cdl_buy"], user_rules["cdl_sell"]
+		for pattern in last_stock_cdl_pattern:
+			if pattern != None:
+				if pattern in cdl_buy:
+					hit_cdl_buy_rules.append(pattern)
+					cdl_buy.remove(pattern)
+				elif pattern in cdl_sell:
+					hit_cdl_sell_rules.append(pattern)
+					cdl_sell.remove(pattern)
+		miss_cdl_buy_rules, miss_cdl_sell_rules = cdl_buy, cdl_sell
+			
 		col_rules_results.insert_one({
 			'ticker': ticker,
 			'hit_buy_rules': hit_buy_rules,
 			'hit_sell_rules': hit_sell_rules,
 			'miss_buy_rules': miss_buy_rules,
-			'miss_sell_rules': miss_sell_rules
+			'miss_sell_rules': miss_sell_rules,
+			'hit_cdl_buy_rules': hit_cdl_buy_rules,
+			'miss_cdl_buy_rules': miss_cdl_buy_rules,
+			'hit_cdl_sell_rules': hit_cdl_sell_rules,
+			'miss_cdl_sell_rules': miss_cdl_sell_rules
 		})
 		
 		print(ticker)
